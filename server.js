c2a454e88370bbbed8dc7c4d8503b33a0cf8096e
ahmadammmoura <ammoura575@gmail.com>
2021-05-17 16:10:45 +0300

    first comment

--- server.js
+++ server.js
@@ -3,21 +3,26 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
 
 const app = express();
 app.use(cors());
 
+mongoose.connect(`mongodb://localhost:${process.env.DATABASE_URL}/books`, {useNewUrlParser: true, useUnifiedTopology: true});
+
+
 const PORT = process.env.PORT || 3001;
 
 app.get('/test', (request, response) => {
 
-  // TODO: 
+  // TODO:
   // STEP 1: get the jwt from the headers
   // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
   // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
   // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
+  response.send('hellosmflsfs')
 
 })
 
