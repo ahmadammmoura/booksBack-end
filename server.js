a03e82fcca44e314c1cd0974e7ba9a87d476f2a5
ahmadammmoura <55942495+ahmadammmoura@users.noreply.github.com>
2021-05-17 15:40:42 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const jwksClient = require('jwks-rsa');
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+app.get('/test', (request, response) => {
+
+  // TODO: 
+  // STEP 1: get the jwt from the headers
+  // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
+  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
+  // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
+
+})
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
