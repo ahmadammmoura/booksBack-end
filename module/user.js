d8c5c36434dbc3297dfdcbf5d393b8afb6977744
ahmadammmoura <ammoura575@gmail.com>
2021-05-17 17:55:14 +0300

    finsh api

---
+++ module/user.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+
+const books = new mongoose.Schema({
+    name: String,
+    description: String,
+    status : String
+  });
+  
+  const user = new mongoose.Schema({
+    Email: String,
+    books: [books]
+  });
+  
+  
+  const UserModel = mongoose.model('user', user);
+  const BooksModel = mongoose.model('books', books);
+
+
+function adduser(){
+    const ahmad = new UserModel({
+        Email: 'ammoura575@gmail.com',
+        books: [
+            {
+                name: 'sofie world',
+                description: `Sophie's World (Norwegian: Sofies verden) is a 1991 novel by Norwegian writer Jostein Gaarder. It follows Sophie Amundsen, a Norwegian teenager who is introduced to the history of philosophy by Alberto Knox, a middle-aged philosopher`,
+                status : 'published'
+            },
+            {
+                name: 'Children of Gebelawi',
+                description: `Children of Gebelawi is a novel by the Egyptian writer and Nobel laureate Naguib Mahfouz. It is also known by its Egyptian dialectal transliteration, Awlad Haretna, and by the alternative translated transliteral Arabic title of Children of Our Alley.`,
+                status : 'published'
+            }
+        ]
+    });
+    const abeer = new UserModel({
+        Email: 'dr.odour@yahoo.com',
+        books: [
+            {
+                name: '1984',
+                author: 'George Orwell',
+                description: 'is a dystopian social science fiction novel',
+            },
+        ]
+    });
+
+
+    ahmad.save();
+    abeer.save();
+}
+
+function doSomething(req,res){
+
+    const {Email}=req.query
+    
+
+    UserModel.find({ Email:Email }, function (err, kittens) {
+        if (err) return console.error(err);
+        // return kittens;
+        res.send(kittens)
+    });
+}
+
+// UserModel.find(function (err, kittens) {
+//     if (err) return console.error(err);
+//     // return kittens;
+//     console.log(kittens);
+// });
+
+// adduser()
+
+
+
+
+module.exports = {
+    adduser: adduser,
+    doSomething: doSomething
+};
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
+const ModuleUser = require('./module/user');
 
 const app = express();
 app.use(cors());
@@ -13,17 +14,22 @@ app.use(cors());
 mongoose.connect(`mongodb://localhost:${process.env.DATABASE_URL}/books`, {useNewUrlParser: true, useUnifiedTopology: true});
 
 
+
+
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
+app.get('/books', (request, response) => {
 
+  ModuleUser.doSomething(request,response)
   // TODO:
   // STEP 1: get the jwt from the headers
   // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
   // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
   // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
-  response.send('hellosmflsfs')
+  // response.send(kittens)
+});
 
-})
+
+
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
